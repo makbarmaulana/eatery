ae02efaa903c0b9be269def8d69be2843d7cb591
M Akbar Maulana <makbarm.id@gmail.com>
2023-07-20 23:12:09 +0700

    Add dotenv-webpack and reconfigure webpack and karma

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  KEY: 'YOUR_API_KEY',
+  KEY: process.env.API_KEY,
   BASE_URL: 'https://restaurant-api.dicoding.dev',
   BASE_IMAGE_URL: (size, pictureId) => `https://restaurant-api.dicoding.dev/images/${size}/${pictureId}`,
   DEFAULT_LANGUAGE: 'en-us',
